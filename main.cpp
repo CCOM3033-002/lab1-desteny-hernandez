99ff97d706ee365d998a463505cec13af21da7e1
Desteny Hernandez <desteny.hernandez1@upr.edu>
2025-02-08 03:02:41 +0000

    comentarios

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@ Lab 1 - Box office
 Nombre: Desteny Hernandez De Juan
 Núm. Est: 401-23-3159
 */
+
 #include <iostream>
 #include <iomanip>
 #include <string>
@@ -10,34 +11,32 @@ using namespace std;
 
 int main()
 {
-     // Declaramos nuestras variables e inicializamos los precios de los tickets.
-     string nombre;
-     double adultPrice = 10.0, childPrice = 6.0, adultAmount, childAmount;
-     char space;
 
-     cout << "This program calculates the Gross and Net Box Office Profit and the Distributer's Profit of a movie playing in the theater.";
+     string nombre;                                                        // Declaramos nombre de la película.
+     double adultPrice = 10.0, childPrice = 6.0, adultAmount, childAmount; // Inicializamos precios de los tickets y declaramos la cantidad de tickets que se van a vender.
+     char space;                                                           // Declaramos variable para crear espacios en el terminal.
 
-     //  Crea una linea de espacio.
+     cout << "This program calculates the Gross and Net Box Office Profit and the Distributer's Profit of a movie playing in the theater."; // Proposito del programa.
+
      cin.ignore();
-     cin.get(space);
+     cin.get(space); //  Crea una linea de espacio.
 
      cout << "Enter the name of the movie: ";
-     getline(cin, nombre);
+     getline(cin, nombre); // Usuario entra nombre de la película.
 
      cout << "Enter the amount of adult tickets sold: ";
-     cin >> adultAmount;
+     cin >> adultAmount; // Usuario entra cantidad de tickets para adultos.
 
      cout << "Enter the amount of child tickets sold: ";
-     cin >> childAmount;
+     cin >> childAmount; // Usuario entra cantidad de tickets para niños.
 
-     //  Crea una linea de espacio.
      cin.ignore();
-     cin.get(space);
+     cin.get(space); //  Crea una linea de espacio.
 
      // Gross sales de tickets para adultos y niños
      double gross = (adultPrice * adultAmount) + (childPrice * childAmount);
 
-     // Net sales de tickets
+     // Net sales de tickets, toma el 20% de gross sales
      double net = gross * 0.2;
 
      // Cantidad pagada al distribuidor
